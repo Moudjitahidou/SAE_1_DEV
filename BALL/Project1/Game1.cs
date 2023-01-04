78347abd81fa0323f96569d836dfc987d3c65876
Moudjitahidou Boinaidi <Moudjitahidou.Boinaidi@etu.univ-smb.fr>
2023-01-04 17:44:45 +0100

    enfin

--- BALL/Project1/Game1.cs
+++ BALL/Project1/Game1.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended.Tiled;
+using MonoGame.Extended.Tiled.Renderers;
 
 namespace Project1
 {
@@ -8,6 +10,9 @@ namespace Project1
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+
+        private TiledMap _tiledMap;
+        private TiledMapRenderer _tiledMapRenderer;
 
         public Game1()
         {
@@ -19,7 +24,8 @@ namespace Project1
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-            //////////////////////////////////////////////////////JE SUIS LAA/////////////////////////////
+            /////////////////////////////////ajoute juste la map///////ligne de code déja présent//////////////// 
+            GraphicsDevice.BlendState = BlendState.AlphaBlend;
 
             base.Initialize();
         }
@@ -29,6 +35,8 @@ namespace Project1
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            _tiledMap = Content.Load<TiledMap>("Map");
+            _tiledMapRenderer = new TiledMapRenderer(GraphicsDevice, _tiledMap);
         }
 
         protected override void Update(GameTime gameTime)
@@ -37,6 +45,7 @@ namespace Project1
                 Exit();
 
             // TODO: Add your update logic here
+            _tiledMapRenderer.Update(gameTime);
 
             base.Update(gameTime);
         }
@@ -46,6 +55,7 @@ namespace Project1
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            _tiledMapRenderer.Draw();
 
             base.Draw(gameTime);
         }
