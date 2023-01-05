78ff2e1f648853a03491f0f35b73e1a8e5f0fcaf
Moudjitahidou Boinaidi <Moudjitahidou.Boinaidi@etu.univ-smb.fr>
2023-01-05 08:11:31 +0100

    pre=ise

---
+++ BALL/Project1/Content/groundGravel.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="groundGravel" tilewidth="16" tileheight="16" tilecount="3328" columns="52">
+ <image source="tileset/groundGravel.png" width="832" height="1024"/>
+</tileset>
--- BALL/Project1/Content/mapGeneraleTuiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="mapGeneraleTuiles" tilewidth="16" tileheight="16" tilecount="350" columns="25">
- <image source="mapGeneraleTuiles.png" width="400" height="224"/>
-</tileset>
