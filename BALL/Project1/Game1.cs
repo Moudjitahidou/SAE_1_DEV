aba1fe915543018d377a28763556a6e52dbc1c19
Moudjitahidou Boinaidi <Moudjitahidou.Boinaidi@etu.univ-smb.fr>
2023-01-05 11:02:43 +0100

    alllll

--- BALL/Project1/Game1.cs
+++ BALL/Project1/Game1.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended.Content;
+using MonoGame.Extended.Serialization;
+using MonoGame.Extended.Sprites;
 using MonoGame.Extended.Tiled;
 using MonoGame.Extended.Tiled.Renderers;
 
@@ -13,6 +16,14 @@ namespace Project1
 
         private TiledMap _tiledMap;
         private TiledMapRenderer _tiledMapRenderer;
+        private KeyboardState _keyboardState;
+
+        private Vector2 _positionPerso;
+        private AnimatedSprite _perso;
+        
+        private int _sensPerso;
+        private int _vitessePerso;
+        float deltaSeconds;
 
         public Game1()
         {
@@ -24,8 +35,9 @@ namespace Project1
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-            /////////////////////////////////ajoute juste la map///////ligne de code déja présent//////////////// 
             GraphicsDevice.BlendState = BlendState.AlphaBlend;
+            _positionPerso = new Vector2(20, 340);
+            _vitessePerso = 100;
 
             base.Initialize();
         }
@@ -37,6 +49,8 @@ namespace Project1
             // TODO: use this.Content to load your game content here
             _tiledMap = Content.Load<TiledMap>("map_tennis");
             _tiledMapRenderer = new TiledMapRenderer(GraphicsDevice, _tiledMap);
+            SpriteSheet spriteSheet = Content.Load<SpriteSheet>("persoAnimation.sf", new JsonContentLoader());
+            _perso = new AnimatedSprite(spriteSheet);
         }
 
         protected override void Update(GameTime gameTime)
@@ -45,7 +59,13 @@ namespace Project1
                 Exit();
 
             // TODO: Add your update logic here
+            deltaSeconds = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            _keyboardState = Keyboard.GetState();
+
             _tiledMapRenderer.Update(gameTime);
+            _perso.Play("frames1"); // une des animations définies dans « persoAnimation.sf »
+            _perso.Update(deltaSeconds); // time écoulé
+            Deplacement();
 
             base.Update(gameTime);
         }
@@ -56,8 +76,43 @@ namespace Project1
 
             // TODO: Add your drawing code here
             _tiledMapRenderer.Draw();
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(_perso, _positionPerso);
+            _spriteBatch.End();
 
             base.Draw(gameTime);
+        }
+        
+        
+        
+        
+        public void Deplacement()
+        {
+            if (_keyboardState.IsKeyDown(Keys.Right) && !(_keyboardState.IsKeyDown(Keys.Left)))
+            {
+                _sensPerso = 1;
+                _positionPerso.X += _sensPerso * _vitessePerso * deltaSeconds;
+
+            }
+            else if ((_keyboardState.IsKeyDown(Keys.Left)) && !(_keyboardState.IsKeyDown(Keys.Right)))
+            {
+                _sensPerso = -1;
+                _positionPerso.X += _sensPerso * _vitessePerso * deltaSeconds;
+
+            }
+            else if ((_keyboardState.IsKeyDown(Keys.Up)) && !(_keyboardState.IsKeyDown(Keys.Down)))
+            {
+                _sensPerso = -1;
+                _positionPerso.Y += _sensPerso * _vitessePerso * deltaSeconds;
+
+            }
+            else if ((_keyboardState.IsKeyDown(Keys.Down)) && !(_keyboardState.IsKeyDown(Keys.Up)))
+            {
+                _sensPerso = 1;
+                _positionPerso.Y += _sensPerso * _vitessePerso * deltaSeconds;
+
+            }
+
         }
     }
 }
